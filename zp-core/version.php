d310a86112b8477aba1c25995e5a52252ed981a2
Renae Bastholm <renae@dizzybee.com.au>
2013-04-30 09:21:18 +0800

    Initial Fresh Install

---
+++ zp-core/version.php
@@ -0,0 +1,5 @@
+<?php // This file contains version info only and is automatically updated. DO NOT EDIT.
+define('ZENPHOTO_VERSION', '1.4.2.4');
+define('ZENPHOTO_RELEASE', 10045);
+define("RELEASE", true);
+?>
